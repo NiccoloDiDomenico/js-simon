5852a06a70d7966c7ff9c04576de6c557c6b83ab
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-11 21:21:05 +0100

    feat: adds user input number

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // ELEMENTS
 const startBtnElement = document.getElementById("start-btn")
 const cpuNumContainerElement = document.getElementById("cpu-num-container")
+const userFormElement = document.getElementById("user-form")
 const userNumContainerElement = document.getElementById("user-num-container")
 const counterElement = document.getElementById("counter")
+const checkBtnElement = document.getElementById("check-btn")
 
-console.log(startBtnElement, cpuNumContainerElement, userNumContainerElement, counterElement);
+console.log(startBtnElement, cpuNumContainerElement, userFormElement, userNumContainerElement, counterElement, checkBtnElement);
 
 
 // INPUTS
@@ -18,30 +20,30 @@ console.log(cpuNumInput, userNumInput);
 // Start Game
 startBtnElement.addEventListener("click", function () {
 
-    // Game section
+    // Cpu number
     startBtnElement.classList.add("d-none")
     counterElement.classList.remove("d-none")
     cpuNumContainerElement.classList.remove("d-none")
 
     // Genera 5 numeri casuali
-    let cpuRandomNumbers = [];
+    let cpuNumbersValues = [];
     for (let i = 0; i < 5; i++) {
         const randomNumbers = Math.floor(Math.random() * 100) + 1;
         // console.log(randomNumbers);
-        cpuRandomNumbers.push(randomNumbers);
+        cpuNumbersValues.push(randomNumbers);
     }
     
     // Assegna i numeri casuali agli input prelevati
     for (let i = 0; i < cpuNumInput.length; i++) {
         if (cpuNumInput[i]) {
-            cpuNumInput[i].value = cpuRandomNumbers[i]
+            cpuNumInput[i].value = cpuNumbersValues[i]
         }
         
     }
-    console.log(`Numeri casuali generati:`, cpuRandomNumbers);
+    console.log(`Numeri casuali generati:`, cpuNumbersValues);
     
     // Setto il timer
-    let timer = 9;
+    let timer = 2;
     console.log(timer);
     
     const intervalID = setInterval(function() {
@@ -51,6 +53,25 @@ startBtnElement.addEventListener("click", function () {
         if (timer < 0) {
             // console.log("debug");
             clearInterval(intervalID)
+
+            // User numb
+            counterElement.classList.add("d-none")
+            cpuNumContainerElement.classList.add("d-none")
+            userNumContainerElement.classList.remove("d-none")
+
+            // User form
+            userFormElement.addEventListener("click", function() {
+                let userNumValues = [];
+
+                for (let i = 0; i < userNumInput.length; i++) {
+                    let currentValue = userNumInput[i].value;
+                    console.log(currentValue);
+                    
+                    userNumValues.push(currentValue)
+                }
+                console.log(userNumValues);
+                
+            })
 
         }
 
