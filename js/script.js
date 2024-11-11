3bbf62d4fe16741bbcd6952753ccebb2709dbc66
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-11 20:11:36 +0100

    feat: add timer

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
 const startBtnElement = document.getElementById("start-btn")
 const cpuNumContainerElement = document.getElementById("cpu-num-container")
 const userNumContainerElement = document.getElementById("user-num-container")
+const counterElement = document.getElementById("counter")
 
-console.log(startBtnElement, cpuNumContainerElement, userNumContainerElement);
+console.log(startBtnElement, cpuNumContainerElement, userNumContainerElement, counterElement);
 
 
 // INPUTS
@@ -18,6 +19,8 @@ console.log(cpuNumInput, userNumInput);
 startBtnElement.addEventListener("click", function () {
 
     // Game section
+    startBtnElement.classList.add("d-none")
+    counterElement.classList.remove("d-none")
     cpuNumContainerElement.classList.remove("d-none")
 
     // Genera 5 numeri casuali
@@ -37,4 +40,20 @@ startBtnElement.addEventListener("click", function () {
     }
     console.log(`Numeri casuali generati:`, cpuRandomNumbers);
     
+    // Setto il timer
+    let timer = 9;
+    console.log(timer);
+    
+    const intervalID = setInterval(function() {
+        counterElement.innerHTML = timer;
+        timer--;
+
+        if (timer < 0) {
+            // console.log("debug");
+            clearInterval(intervalID)
+
+        }
+
+    }, 1000);
+
 })
